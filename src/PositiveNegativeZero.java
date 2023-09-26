aabfb6b16f93030c5337986e54fa3b491cbe4b92
Chris <c.r.zambito@gmail.com>
2023-09-26 18:18:37 -0400

    positive negative zero

---
+++ src/PositiveNegativeZero.java
@@ -0,0 +1,16 @@
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+
+    }
+
+    // method checkNumber
+    public static void checkNumber(int number) {
+        String result = "zero";
+        if (number > 0) {
+            result = "positive";
+        } else if (number < 0) {
+            result = "negative";
+        }
+        System.out.println(result);
+    }
+}
